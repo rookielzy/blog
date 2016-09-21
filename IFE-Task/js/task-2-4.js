b205b05ce9ae75786bcdf44348350fe282f2ac27
rookielzy <slimjetlzy@gmail.com>
2016-09-21 11:18:12 +0800

    task-2-4 without delete data

--- IFE-Task/js/task-2-4.js
+++ IFE-Task/js/task-2-4.js
@@ -12,15 +12,40 @@ var aqiData = {};
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
-function addAqiData() {
+  var cityInput = document.getElementById("aqi-city-input");
+      airInput = document.getElementById("aqi-value-input");
 
+function addAqiData() {
+  var city = city.value.trim(),
+      airValue = airInput.value.trim();
+
+  // 最好加上输入处理
+  aqiData[city] = airvalue;
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
-
+  var table = document.getElementById("aqi-table");
+  var tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+  var key = Object.keys(aqiData),
+      value = Object.value(aqiData);
+  // create lines
+  for (var i = 0; i < aqiData.length; i++){
+    var row = document.createElement("tr");
+    tbody.appendChild(row);
+    var cell1 = document.createElement("td");
+    cell1.innerText = (key[i]);
+    row.appendChild(cell1);
+    var cell2 = document.createElement("td");
+    cell2.innerText = (value[i]);
+    row.appendChild(cell2);
+    var cell3 = document.createElement("td");
+    cell3.innerHTML = ("<button>Delete</button>");
+    row.appendChild(cell3);    
+  }
 }
 
 /**
@@ -45,7 +70,7 @@ function delBtnHandle() {
 function init() {
 
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-
+  document.getElementById("add-btn").addEventListener("click", addBtnHandle);
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
 
 }
