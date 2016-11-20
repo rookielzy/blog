ec207384b40212da9223c9380d49adccbd975811
magicRibs <slimjetlzy@gmail.com>
2016-11-21 00:11:10 +0800

    decimal to roman number

--- FCC/javascripts/IntermediateAlgorithmScripting/RomanNumberConverter.js
+++ FCC/javascripts/IntermediateAlgorithmScripting/RomanNumberConverter.js
@@ -1,41 +1,31 @@
 
 function convertToRoman(num) {
 
-    // Create arrays with default conversion with matching indices.
-    var decimalValue = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
-    var romanNumeral = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+    // Create two particular arrays to store decimal numbers and roman number
+    var numValue = [1,    4,    5,    9,   10,  40,  50,   90,  100,  400, 500,  900, 1000];
+    var romanNum = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+    var numTmp = num;
+    var roman = '';
 
-    // Create a copy of num to work on and an empty string variable for the final roman number
-    var numCopy = num;
-    var romanized = '';
-
-    // While the decimal number is greater than 0,
-    while (numCopy > 0) {
-        // Loop through the indices of the decimalValue array.
-        for (var index = 0; index < decimalValue.length; index++) {
-            if (numCopy === 1000) {
-                romanized = 'M';
-                break;
-            }       
-            while (numCopy > 1000) {
-                romanized += 'M';
-                numCopy -= 1000;
-            }
-            if (numCopy === 0) {
-                break;
-            }
-
-            // Get the maximum decimal number less or equal than the decimal number.
-            if (decimalValue[index] <= numCopy && +decimalValue[index + 1] > numCopy) {
-                // Add the Roman numeral & decrease numCopy by the decimal equivalent.
-                romanized += romanNumeral[index];
-                numCopy -= decimalValue[index];
+    // while argument is greater or equal than 1000
+    while (numTmp >= 1000) {
+        roman += 'M';
+        numTmp -= 1000;
+    }
+    // While the argument is greater than 0,
+    while (numTmp > 0) {
+      // loop the numValue
+        for (var i = 0; i < numValue.length; i++) {
+            // eg: numTmp = 987, it is between 900 and 1000
+            if (numValue[i] <= numTmp && numValue[i + 1] > numTmp) {
+                roman += romanNum[i];
+                // than minus 900, numTmp === 87, and break to the next loop
+                numTmp -= numValue[i];
                 break;
             }
         }
     }
-
-    return romanized;
+    return roman;
 }
 
 convertToRoman(1004);
