05ba914acdff01091477b255e66b8612a3ccbc25
rookielzy <slimjetlzy@gmail.com>
2016-12-08 17:36:26 +0800

    to Shadow

--- HTML5/README.md
+++ HTML5/README.md
@@ -2,3 +2,25 @@
 
 ## Chapter One
 1. You can not set canvas's size by using px
+
+### Base API
+1. fillStyle = ('color')
+2. strokeStyle = ('color')
+
+## Chapter Two
+
+### Rect API
+1. clearRect(double x, double y, double w, double h)
+2. strokeRect(double x, double y, double w, double h)
+3. fillRect(double x, double y, double w, double h)
+
+### Gradient API
+1. createLinearGradient(double x, double y, double row, double column)
+2. createRedialGradient(double x0, double y0, double y0, double x1, double y1, double r1)
+3. addColorStop(precent, 'color')
+
+### Pattern API
+1. createPattern(image, repeat | repeat-x | repeat-y | no-repeat);
+2. image = new Image(); image.src = ' '; image.onload = function() { }
+
+### Shadow API
---
+++ HTML5/SimpleRect/rect.js
@@ -0,0 +1,40 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+
+// Simple Rect
+// context.lineJoin = 'round';
+// context.lineWidth = 30;
+
+// context.font = '24px Helvetica';
+// context.fillText('Click anuwhere to erase', 175, 40);
+
+// context.strokeStyle = 'goldenrod';
+// context.fillStyle = 'rgba(0, 0, 255, 0.5)';
+
+// context.strokeRect(75, 100, 200, 200);
+// context.fillRect(325, 100, 200, 200);
+
+// context.canvas.onmousedown = function(e) {
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+// };
+
+// Gradient
+// var gradient = context.createLinearGradient(0, 0, canvas.width, 0);
+// var gradient = context.createRadialGradient(canvas.width/2, canvas.height, 10, canvas.width/2, 0, 100);
+// gradient.addColorStop(0, 'blue');
+// gradient.addColorStop(0.25, 'white');
+// gradient.addColorStop(0.5, 'purple');
+// gradient.addColorStop(0.75, 'red');
+// gradient.addColorStop(1, 'yellow');
+
+// context.fillStyle = gradient;
+// context.fillRect(0, 0, canvas.width, canvas.height);
+image = new Image();
+image.src = "icon1.gif"
+image.onload = function() {
+    var pattern = context.createPattern(image, 'repeat-x');
+    context.fillStyle = pattern;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fill();
+};
