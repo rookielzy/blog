d45a96222a861d86b3ebc78c84497eb91f686d2c
rookielzy <slimjetlzy@gmail.com>
2016-12-12 23:23:17 +0800

    SPA first commit

---
+++ JavaScripts30/SPA/main.js
@@ -0,0 +1,19 @@
+// jslint settings
+
+// Module /spa/
+// Provides char slider capability
+    // Module scope variables
+        // Set constants
+        // Declare all other module scope variables
+    // DOM method /toggleSlider/
+    // alternates slider height
+
+    // Event handler /onClickSlider/
+    // receives click event and calls toggleSlider
+
+    // Public method /initModule/
+    // sets initial state and provides feature
+        // render HTML
+        // initialize slider height and title
+        // bind the user click event to the handler
+// Start spa once DOM is ready
