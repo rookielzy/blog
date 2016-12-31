dbd9f9d28b9fd858ba1d6e86a63c4b004a0c7a00
rookielzy <slimjetlzy@gmail.com>
2016-12-31 13:57:16 +0800

    click function all done! todo 时钟动态效果

--- FCC/javascripts/PomodoroClock.js
+++ FCC/javascripts/PomodoroClock.js
@@ -1,12 +1,11 @@
 var clock = document.getElementById('clock');
 
-clock.addEventListener('click', function() {
+clock.addEventListener('click', function(min) {
     // console.log(event.target);
     var timeLeft = document.querySelectorAll('span')[3];
     // console.log(timeLeft);
     var date = new Date();
-    date.setMinutes(25, 00);
-    var min = date.getMinutes();
+    date.setMinutes(min, 00);
     var sec = date.getSeconds();
     var run = setInterval(function() {
         if (sec !== 0) {
@@ -25,3 +24,60 @@ clock.addEventListener('click', function() {
     }, 1000);
 
 }, false);
+
+function changeTimeLength(input, index) {
+    // console.log(input);
+    // console.log(index);
+    var timeSpan = document.querySelectorAll('span');
+    // console.log(timeSpan);
+    if (input.value === '-') {
+        input.addEventListener('click', function() {
+            if (index === 0) {
+                var value = parseInt(timeSpan[0].innerText);
+                if (value === 0) {
+                    return;
+                } else {
+                    value--;
+                }
+                timeSpan[0].innerText = value;
+            } else {
+                var value = parseInt(timeSpan[1].innerText);
+                if (value === 0) {
+                    return;
+                } else {
+                    value--;
+                }
+                timeSpan[1].innerText = value;
+                timeSpan[3].innerHTML = value + ':' + '00';                
+            }
+            // console.log(timeSpan[index]);
+        }, false);
+    } else {
+        input.addEventListener('click', function() {
+            if (index === 1) {
+                var value = parseInt(timeSpan[0].innerText);
+                if (value === 0) {
+                    return;
+                } else {
+                    value++;
+                }
+                timeSpan[0].innerText = value;
+            } else {
+                var value = parseInt(timeSpan[1].innerText);
+                if (value === 0) {
+                    return;
+                } else {
+                    value++;
+                }
+                timeSpan[1].innerText = value;
+                timeSpan[3].innerHTML = value + ':' + '00';
+            }
+        }, false);
+    }
+}
+
+window.onload = function() {
+    var inputs = document.querySelectorAll('input');
+    // console.log(inputs);
+    inputs.forEach(changeTimeLength);
+};
