36b1607baa54cb21962d5c1a044433a29ae68f20
unknown <>
2017-01-08 22:46:59 +0800

    Debug Dev

---
+++ JavaScripts30/DebugDev/main.js
@@ -0,0 +1,30 @@
+var p = document.querySelector('p');
+p.addEventListener('click', function(){
+    p.style.color = 'blue';
+}, false);
+
+console.log(p);
+
+console.dir(p);
+
+console.assert(1 === 1, 'wtf');
+
+console.clear();
+
+console.debug('debug what', 1===1);
+
+console.error('error', 1===2);
+
+console.group(p);
+// console.groupCollapsed(p);
+console.info('some info', 1===2);
+console.info('some info', 1===1);
+console.groupEnd(p);
+console.info('some info', 1===3);
+console.warn('you are wrong', 1===2);
+console.time();
+var i = 1;
+var j = i + 2;
+console.timeEnd();
+
+
