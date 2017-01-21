6fd9c75d7e9f7b3925024fb11515416741866ebc
rookielzy <slimjetlzy@gmail.com>
2017-01-21 11:28:14 +0800

    Sign up 2nd version

--- DailyUI100Days/Day1Signup/main.js
+++ DailyUI100Days/Day1Signup/main.js
@@ -2,19 +2,12 @@ const signupBtn = document.querySelector('#signupBtn');
 const show = document.querySelector('.show');
 const signup = document.querySelector('.signup');
 
-const showHtml = show.innerHTML;
-show.style.display = 'none';
+// const showHtml = show.innerHTML;
+// show.style.display = 'none';
 // show.innerHTML = '';
 // console.log(showHtml);
 // console.log(signup.classList);
 signupBtn.addEventListener('click', function() {
-    if (signup.classList[1]) {
-        show.style.display = 'none';
-        signup.classList = 'signup';
-        // show.innerHTML = '';
-    } else {
-        // show.innerHTML = showHtml;
-        show.style.display = 'block';
-        signup.classList += ' active';
-    }
+    signup.classList += ' active';
+    signupBtn.innerHTML = '<h1>Submit</h1>'
 }, false);
