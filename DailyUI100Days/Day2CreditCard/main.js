8e666856258c20950239c3e789c8812a3b762e71
rookielzy <slimjetlzy@gmail.com>
2017-01-22 16:03:21 +0800

    Switch Cards Success!

--- DailyUI100Days/Day2CreditCard/main.js
+++ DailyUI100Days/Day2CreditCard/main.js
@@ -2,20 +2,11 @@ const cards = document.querySelectorAll('.cards-flow div');
 
 cards.forEach((card, index) => {
     card.addEventListener('click', function() {
+        const card4 = document.querySelector('.card4');
+        lastClassName = this.className;
         this.className = 'card4';
-        this.style.zIndex = 100;
-        var nextBro = this.nextElementSibling;
-        while (nextBro) {
-            console.log(typeof nextBro.className);
-            if (index > 3) {
-                if (index == 5) {
-                    nextBro.className = 'card1';
-                    nextBro.style.zIndex = -1;
-                } else {
-                    nextBro.className = 'card' + (index+3); 
-                }
-            }
-            this = nextBro;
-        }
+        // this.style.zIndex = 100;
+        card4.className = lastClassName;
     }, false);
 });
+
