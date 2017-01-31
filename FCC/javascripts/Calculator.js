67d5798236009f93ac1936cc132de4e608a85337
rookielzy <slimjetlzy@gmail.com>
2017-01-31 21:45:27 +0800

    Drink too much. Take a rest!

--- FCC/javascripts/Calculator.js
+++ FCC/javascripts/Calculator.js
@@ -1,8 +1,17 @@
-const test = document.querySelectorAll('.first-part div');
-// console.log(test);
-test.forEach(btn => {
+const btns = document.querySelectorAll('.first-part div, #one > div > div, #equal');
+// console.log(btns);
+const displayMain = document.querySelector('#displayMain');
+const displayAll = document.querySelector('#displayAll');
+btns.forEach(btn => {
     btn.addEventListener('mousedown', function() {
         btn.classList += ' pressdown';
+        if (displayMain.innerText.length >= 9) {
+            displayMain.innerText = '';
+            displayAll.innerText = 'OUT OF LIMIT!'
+        } else {
+            displayMain.innerText = btn.innerText;
+            displayAll.innerText += btn.innerText;
+        }
     }, false);
     btn.addEventListener('mouseup', function() {
         btn.classList -= ' pressdown';
