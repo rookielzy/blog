b84423157980e544beb99ed8c1fd77806e43cdb5
rookielzy <slimjetlzy@gmail.com>
2017-02-28 19:44:55 +0800

    Frog Game Done!

--- PractiseProject/ForgGame/js/app.js
+++ PractiseProject/ForgGame/js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    // loop the enemies 
+    if (this.x >= 505) {
+        this.x = 0;
+    }
+
+    // Check for collision with enemies or barrier-walls
+    checkCollision(this);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +35,101 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var Player = function(x ,y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = "images/char-boy.png";
+}
+
+Player.prototype.update = function() {
 
 }
 
-Player.prototype.update
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    displayScoreLevel(score, gameLevel);
+}
+
+Player.prototype.handleInput = function(keyPress) {
+    switch(keyPress) {
+        case 'left':
+            player.x -= player.speed;
+            break;
+        case 'right':
+            player.x += player.speed;
+            break;
+        case 'up':
+            player.y -= player.speed - 20;
+            break;
+        case 'down':
+            player.y += player.speed - 20;
+            break;
+        default:
+            break;
+    }
+    console.log('keyPress is: ' + keyPress);
+}
+
+// Display Player's  score
+var displayScoreLevel = function(aScore, aLevel) {
+    var canvas = document.getElementsByTagName('canvas');
+    var firstCanvasTag = canvas[0];
+
+    // add player score and level to div element created
+    scoreLevelDiv.innerHTML = 'Score: ' + aScore + ' / ' + 'Level: ' + aLevel;
+    document.body.insertBefore(scoreLevelDiv, firstCanvasTag[0]);
+};
+
+var checkCollision = function(anEnemy) {
+    // check for collision between enemy and player
+    if (
+        player.y + 131 >= anEnemy.y + 90
+        && player.x + 25 <= anEnemy.x + 88
+        && player.y + 73 <= anEnemy.y + 135
+        && player.x + 76 >= anEnemy.x + 11) {
+        console.log('collided');
+        player.x = 202.5;
+        player.y = 383;
+    }
+
+    if (player.y + 63 <= 0) {
+        player.x = 202.5;
+        player.y = 383;
+        console.log('you made it!');
+
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 505, 171);
+        
+        score += 1;
+        gameLevel += 1;
+        increaseDifficulty(score);
+    }
+    // check if player runs into left, bottom, or right canvas walls
+    // prevent player from moving beyond canvas wall boundaries
+    if (player.y > 383 ) {
+        player.y = 383;
+    }
+    if (player.x > 402.5) {
+        player.x = 402.5;
+    }
+    if (player.x < 2.5) {
+        player.x = 2.5;
+    }
+};
+
+// Increase number of enemies on screen based on player's score
+var increaseDifficulty = function(numEnemies) {
+    // remove all previous enemies on canvas
+    allEnemies.length = 0;
+
+    // load new set of enemies
+    for (var i = 0; i <= numEnemies; i++) {
+        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+        
+        allEnemies.push(enemy);
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -38,6 +139,14 @@ Player.prototype.update
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+var allEnemies = [];
+var player = new Player(202.5, 383, 50);
+var score = 0;
+var gameLevel = 1;
+var scoreLevelDiv = document.createElement('div');
+var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+
+allEnemies.push(enemy);
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
